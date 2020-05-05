2761e1a1bbdf02689251f72dfbf4633c97965dd3
Daniel Kiss <littlesnorrboy@gmail.com>
2020-05-05 22:34:19 +0200

    Add a utility to calculate the bounding box of a circle

--- simulation/src/model/geometry/mod.rs
+++ simulation/src/model/geometry/mod.rs
@@ -3,3 +3,14 @@ pub mod point3;
 
 pub use point::*;
 pub use point3::*;
+
+pub fn aabb_over_circle(center: Point, radius: u32) -> (Point, Point) {
+    use crate::tables::SpatialKey2d;
+    let [x, y] = center.as_array();
+    let radius = radius as i32;
+    let from = Point::new(x - radius, y - radius);
+    let to = Point::new(x + radius, y + radius);
+
+    (from, to)
+}
+
