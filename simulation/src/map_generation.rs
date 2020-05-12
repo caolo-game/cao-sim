b8f0b905cc33e655b1c9841a29bd937925a4ee86
Daniel Kiss <littlesnorrboy@gmail.com>
2020-05-12 22:24:13 +0200

    Check for NaN

--- simulation/src/map_generation.rs
+++ simulation/src/map_generation.rs
@@ -219,7 +219,7 @@ pub fn generate_room(
             grad -= min_grad;
             grad /= max_grad - min_grad;
 
-            if grad <= 0.33 {
+            if grad <= 0.33 || grad.is_nan() {
                 return None;
             }
             let terrain = if grad < 0.6 {
