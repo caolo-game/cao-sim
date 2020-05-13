fe64777c2e5b144d025410b4363a38b205fd958e
Daniel Kiss <littlesnorrboy@gmail.com>
2020-05-13 21:21:09 +0200

    Commit to a hybrid design, parallel quicksort and serial radix sort

--- simulation/src/tables/morton/sorting.rs
+++ simulation/src/tables/morton/sorting.rs
@@ -1,15 +1,98 @@
 use super::morton_key::MortonKey;
 use super::msb_de_bruijn;
-use rayon::prelude::*;
 
-const RADIX_MASK_LEN: u8 = 5;
+const RADIX_MASK_LEN: usize = 4;
 const RADIX_MASK: u32 = (1 << (RADIX_MASK_LEN + 1)) - 1;
 const NUM_BUCKETS: usize = RADIX_MASK as usize + 1;
 
-// TODO: create a Sorter struct that holds the temp buffer, so MortonTable may hold one and only
-// allocate these once per lifetime, should speed up rebuilds
+pub fn sort<Pos: Send + Clone, Row: Send + Clone>(
+    keys: &mut [MortonKey],
+    positions: &mut [Pos],
+    values: &mut [Row],
+) {
+    debug_assert!(
+        keys.len() == positions.len(),
+        "{} {}",
+        keys.len(),
+        positions.len()
+    );
+    debug_assert!(
+        keys.len() == values.len(),
+        "{} {}",
+        keys.len(),
+        values.len()
+    );
+    if keys.len() < 2 {
+        return;
+    }
+    if keys.len() < 2050 {
+        sort_radix(keys, positions, values);
+        return;
+    }
+    let pivot = sort_partition(keys, positions, values);
+    let (klo, khi) = keys.split_at_mut(pivot);
+    let (plo, phi) = positions.split_at_mut(pivot);
+    let (vlo, vhi) = values.split_at_mut(pivot);
+    rayon::join(
+        || sort(klo, plo, vlo),
+        || sort(&mut khi[1..], &mut phi[1..], &mut vhi[1..]),
+    );
+}
 
-pub fn sort<Pos: Clone, Row: Clone>(
+/// Assumes that all 3 slices are equal in size.
+/// Assumes that the slices are not empty
+fn sort_partition<Pos, Row>(
+    keys: &mut [MortonKey],
+    positions: &mut [Pos],
+    values: &mut [Row],
+) -> usize {
+    debug_assert!(!keys.is_empty());
+
+    macro_rules! swap {
+        ($i: expr, $j: expr) => {
+            keys.swap($i, $j);
+            positions.swap($i, $j);
+            values.swap($i, $j);
+        };
+    };
+
+    let len = keys.len();
+    let lim = len - 1;
+
+    let (pivot, pivot_ind) = {
+        use std::mem::swap;
+        // choose the median of the first, middle and last elements as the pivot
+
+        let mut first = 0;
+        let mut last = lim;
+        let mut median = len / 2;
+
+        if keys[last] < keys[median] {
+            swap(&mut median, &mut last);
+        }
+        if keys[last] < keys[first] {
+            swap(&mut last, &mut first);
+        }
+        if keys[median] < keys[first] {
+            swap(&mut median, &mut first);
+        }
+        (keys[median], median)
+    };
+
+    swap!(pivot_ind, lim);
+
+    let mut i = 0; // index of the last item <= pivot
+    for j in 0..lim {
+        if keys[j] < pivot {
+            swap!(i, j);
+            i += 1;
+        }
+    }
+    swap!(i, lim);
+    i
+}
+
+fn sort_radix<Pos: Clone, Row: Clone>(
     keys: &mut [MortonKey],
     positions: &mut [Pos],
     values: &mut [Row],
@@ -30,20 +113,27 @@ pub fn sort<Pos: Clone, Row: Clone>(
         return;
     }
 
-    let mut input: Vec<(MortonKey, usize)> = keys
+    let mut buffa: Vec<_> = keys
         .iter()
         .cloned()
         .enumerate()
         .map(|(k, v)| (v, k))
         .collect();
-    let mut output = vec![Default::default();keys.len()];
+    let mut buffb = vec![Default::default(); keys.len()];
+    let mut buffind = 0;
+
+    // let mut counts = vec![Default::default();
 
     let msb = keys.iter().map(|k| msb_de_bruijn(k.0)).max().unwrap();
     // TODO: optimize using min lsb?
     for k in (0..=msb).step_by(RADIX_MASK_LEN as usize) {
-        radix_pass(k as u8, &input[..], &mut output);
-        debug_assert_eq!(input.len(), output.len());
-        std::mem::swap(&mut input, &mut output);
+        if buffind == 0 {
+            radix_pass(k as u8, &buffa[..], &mut buffb);
+        } else {
+            radix_pass(k as u8, &buffb[..], &mut buffa);
+        }
+        debug_assert_eq!(buffa.len(), buffb.len());
+        buffind = 1 - buffind;
     }
 
     // TODO: calculate minimum swaps required
@@ -53,8 +143,8 @@ pub fn sort<Pos: Clone, Row: Clone>(
     let mut ps = Vec::with_capacity(keys.len());
     let mut vs = Vec::with_capacity(keys.len());
 
-    // input and output were swapped so iter on input
-    for (_, i) in input.iter() {
+    let output = if buffind == 0 { buffa } else { buffb };
+    for (_, i) in output.iter() {
         let i = *i;
         ks.push(keys[i]);
         ps.push(positions[i].clone());
@@ -71,62 +161,37 @@ fn radix_pass(
     keys: &[(MortonKey, usize)], // key, index pairs
     out: &mut [(MortonKey, usize)],
 ) {
-    let step = keys.len() / rayon::current_num_threads() + 1;
+    let mut buckets = [0; NUM_BUCKETS];
     // compute the length of each bucket
-    let mut counts = keys
-        .par_chunks(step)
-        .map(|chunk| {
-            let mut buckets = [0; NUM_BUCKETS];
-            for (key, _) in chunk {
-                let bucket = compute_bucket(k, key);
-                buckets[bucket] += 1;
-            }
-            // we'll use the index of the first item as a unique id for each chunk
-            (chunk[0].1, buckets)
-        })
-        .collect::<Vec<_>>();
+    keys.iter().for_each(|(key, _)| {
+        let bucket = compute_bucket(k, key);
+        buckets[bucket] += 1;
+    });
 
     // set the output offsets for each bucket
     // this will indicate the 1 after the last index a chunk will occupy
     let mut base = 0;
     for b_ind in 0..NUM_BUCKETS {
-        let b_ind = b_ind as usize;
-        for buckets in counts.iter_mut() {
-            buckets.1[b_ind] += base;
-            base = buckets.1[b_ind];
-        }
+        buckets[b_ind] += base;
+        base = buckets[b_ind];
     }
 
     // write the output
     debug_assert_eq!(keys.len(), out.len());
-    let outptr = out.as_mut_ptr();
-    let outptr = UnsafeMortonPtr(outptr);
 
-    keys.par_chunks(step).for_each(|chunk| {
-        // find the `buckets` of this chunk
-        let mut buckets = counts
-            .iter()
-            .find(|(c, _)| c == &chunk[0].1)
-            .map(|(_, b)| b)
-            .cloned()
-            .expect("bucket counts of chunk");
-
-        for (key, id) in chunk.iter().rev() {
-            let bucket = compute_bucket(k, key);
-            let index = buckets[bucket] - 1;
-            debug_assert!(index < out.len());
-            unsafe {
-                *outptr.0.add(index) = (*key, *id);
-                buckets[bucket] -= 1;
-            }
-        }
+    keys.iter().rev().for_each(|(key, id)| {
+        let bucket = compute_bucket(k, key);
+        buckets[bucket] -= 1;
+        let index = buckets[bucket];
+        debug_assert!(index < out.len());
+        out[index] = (*key, *id);
     });
 }
 
 #[derive(Clone, Copy)]
-struct UnsafeMortonPtr(*mut (MortonKey, usize));
-unsafe impl Send for UnsafeMortonPtr {}
-unsafe impl Sync for UnsafeMortonPtr {}
+struct UnsafePtr<T>(*mut T);
+unsafe impl<T> Send for UnsafePtr<T> {}
+unsafe impl<T> Sync for UnsafePtr<T> {}
 
 fn compute_bucket(k: u8, key: &MortonKey) -> usize {
     let mask = RADIX_MASK << k;
