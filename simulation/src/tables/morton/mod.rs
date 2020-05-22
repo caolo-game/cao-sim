3f59dda883e38c405787cd2debba5bb74a27c687
Daniel Kiss <littlesnorrboy@gmail.com>
2020-05-22 09:58:45 +0200

    Update map_generation to use noise layering

--- simulation/src/tables/morton/mod.rs
+++ simulation/src/tables/morton/mod.rs
@@ -512,9 +512,9 @@ where
 
     /// Merge two `MortonTable`s by inserting all points that are in `other` but not in `self` and
     /// calling `update` with all points that are present in both tables.
-    pub fn merge<F>(&mut self, other: &Self, update: F) -> Result<(), ExtendFailure<Pos>>
+    pub fn merge<F>(&mut self, other: &Self, mut update: F) -> Result<(), ExtendFailure<Pos>>
     where
-        F: Fn(&Pos, &Row, &Row) -> Row,
+        F: FnMut(&Pos, &Row, &Row) -> Row,
     {
         let inserts = {
             let mut lhs = self.iter_mut();
