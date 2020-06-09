d79fbfc0d6e9ee7066efc9344c27be4eb726a276
Daniel Kiss <littlesnorrboy@gmail.com>
2020-06-09 19:27:50 +0200

    Restore `offset_end`

--- test-env/src/lib.rs
+++ test-env/src/lib.rs
@@ -52,7 +52,7 @@ impl MapRender {
                 .map(|p| RoomConnection {
                     direction: *p,
                     offset_start: 5,
-                    length: 2,
+                    offset_end: 6,
                 })
                 .collect::<Vec<_>>(),
             (UnsafeView::from_table(&mut self.map),),
