f7adb5df53aec703c8122673a49ab37827c1f93c
Daniel Kiss <littlesnorrboy@gmail.com>
2020-06-16 17:50:57 +0200

    Export more stuff

--- simulation/src/prelude.rs
+++ simulation/src/prelude.rs
@@ -1,5 +1,6 @@
 pub use crate::data_store::World;
+pub use crate::model::components;
+pub use crate::model::indices::*;
 pub use crate::storage::views::*;
 pub use crate::storage::*;
-pub use crate::tables::Component;
-pub use crate::tables::Table;
+pub use crate::tables::{self, Component, Table};
