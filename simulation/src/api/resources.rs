9b58fe4456d7c9c6590a9e23dd43c166175e743d
Daniel Kiss <littlesnorrboy@gmail.com>
2020-06-27 20:58:43 +0200

    Rework find_closest_resource_by_range to search the room

--- simulation/src/api/resources.rs
+++ simulation/src/api/resources.rs
@@ -1,9 +1,9 @@
 use super::*;
-use crate::components::{EntityComponent, PositionComponent, ResourceComponent};
+use crate::components::{EntityComponent, PositionComponent, ResourceComponent, RoomProperties};
+use crate::geometry::Axial;
+use crate::model::indices::EmptyKey;
 use crate::model::WorldPosition;
 use crate::profile;
-
-pub const MAX_SEARCH_RADIUS: u32 = 256;
 
 /// Return OperationResult and an EntityId if the Operation succeeded
 pub fn find_closest_resource_by_range(
@@ -28,14 +28,45 @@ pub fn find_closest_resource_by_range(
         }
     };
 
-    let mut candidates = Vec::with_capacity(MAX_SEARCH_RADIUS as usize * 2);
+    let radius = storage
+        .view::<EmptyKey, RoomProperties>()
+        .unwrap_value()
+        .radius;
+
     let WorldPosition { room, pos } = position.0;
+
+    let mut candidate: Option<(Axial, EntityId)> = None;
+
+    let resources = storage.view::<EntityId, ResourceComponent>();
     storage
         .view::<WorldPosition, EntityComponent>()
         .reborrow()
         .table
         .get_by_id(&room)
-        .map(|room| room.find_by_range(&pos, MAX_SEARCH_RADIUS, &mut candidates))
+        // search the whole room
+        .map(|room| {
+            let candidate = &mut candidate;
+            room.query_range(
+                &Axial::new(radius as i32, radius as i32),
+                radius,
+                &mut |pp, entity| {
+                    let resource = resources.get_by_id(&entity.0);
+                    // filter only entities that are 'resources'
+                    if resource.is_some() {
+                        // find the one with min distance
+                        *candidate = candidate
+                            .map(|current| {
+                                if pos.hex_distance(current.0) < pos.hex_distance(pp) {
+                                    current
+                                } else {
+                                    (pp, entity.0)
+                                }
+                            })
+                            .or_else(|| Some((pp, entity.0)));
+                    }
+                },
+            )
+        })
         .ok_or_else(|| {
             warn!(
                 "find_closest_resource_by_range called on invalid room {:?}",
@@ -44,18 +75,12 @@ pub fn find_closest_resource_by_range(
             ExecutionError::InvalidArgument
         })?;
 
-    let resources = storage.view::<EntityId, ResourceComponent>();
-
-    candidates.retain(|(_pos, entity_id)| resources.get_by_id(&entity_id.0).is_some());
-    match candidates
-        .iter()
-        .min_by_key(|(pos, _)| pos.hex_distance(*pos))
-    {
+    match candidate {
         None => {
             debug!("No resource was found");
             vm.set_value(OperationResult::OperationFailed)?;
         }
-        Some((_pos, entity)) => {
+        Some((_, entity)) => {
             let id = entity.0; // move out of the result to free the storage borrow
             vm.set_value(id)?;
             vm.set_value(OperationResult::Ok)?;
