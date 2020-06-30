cf60fad122e3c6bdcf9f047289e388ba92245382
Daniel Kiss <littlesnorrboy@gmail.com>
2020-06-30 21:27:02 +0200

    Add a basic query macro

--- simulation/src/tables/morton/mod.rs
+++ simulation/src/tables/morton/mod.rs
@@ -13,9 +13,9 @@ mod sorting;
 #[cfg(test)]
 mod tests;
 
-pub use self::morton_key::*;
 pub use self::litmax_bigmin::msb_de_bruijn;
 use self::litmax_bigmin::round_down_to_one_less_than_pow_two;
+pub use self::morton_key::*;
 pub use self::serde::*;
 use super::*;
 use litmax_bigmin::litmax_bigmin;
