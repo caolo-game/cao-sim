cfc1954232f6d58980a143158f3a8712236c2a33
Daniel Kiss <littlesnorrboy@gmail.com>
2020-08-20 13:03:08 +0200

    Add join! macro for iterators

--- simulation/src/storage/macros.rs
+++ simulation/src/storage/macros.rs
@@ -56,6 +56,75 @@ macro_rules! query {
     };
 }
 
+///
+///# Examples
+///
+///## Join iterators
+///
+///```
+/// use caolo_sim::query;
+/// use caolo_sim::init_inmemory_storage;
+/// use caolo_sim::components::*;
+/// use caolo_sim::geometry::Axial;
+/// use caolo_sim::model::EntityId;
+///
+/// // these rows are mandatory
+/// use caolo_sim::join;
+/// use caolo_sim::tables::JoinIterator;
+///
+/// let mut store = init_inmemory_storage(None);
+///
+/// let entity_1 = store.insert_entity();
+/// let entity_2 = store.insert_entity();
+/// let entity_3 = store.insert_entity();
+///
+/// query!(
+///     mutate
+///     store
+///     {
+///         EntityId, Bot, .insert_or_update(entity_1, Bot);
+///         EntityId, Bot, .insert_or_update(entity_2, Bot);
+///
+///         EntityId, PositionComponent, .insert_or_update(entity_1, PositionComponent::default());
+///         EntityId, PositionComponent, .insert_or_update(entity_2, PositionComponent::default());
+///         EntityId, PositionComponent, .insert_or_update(entity_3, PositionComponent::default());
+///
+///         // notice how entity_3 is not a bot, but has carry
+///
+///         EntityId, CarryComponent,
+///                  .insert_or_update(entity_1, CarryComponent{carry: 12, carry_max: 69});
+///         EntityId, CarryComponent,
+///                  .insert_or_update(entity_2, CarryComponent{carry: 30, carry_max: 69});
+///         EntityId, CarryComponent,
+///                  .insert_or_update(entity_3, CarryComponent{carry: 40, carry_max: 69});
+///     }
+/// );
+///
+/// let bot_table = store.view::<EntityId, Bot>();
+/// let bot = bot_table.iter();
+/// let pos_table = store.view::<EntityId, PositionComponent>();
+/// let pos = pos_table.iter();
+/// let carry_table = store.view::<EntityId, CarryComponent>();
+/// let car = carry_table.iter();
+///
+/// let res: i32 = join!( 
+///     [ bot: Bot
+///     , pos: PositionComponent
+///     , car: CarryComponent ]
+/// )
+///     // data has fields `carry` and `bot`, specified in the macro invocation
+///     // these are references to their respective components...
+///     // we'll extract the carry amount
+///     //
+///     // pos_components are default (0,0), we access them for demo purposes...
+///     .map(|(id, data)|{ data.car.carry as i32 + data.pos.0.pos.q })
+///     .sum();
+///
+/// assert_eq!(res, 42); // entity_1 carry + entity_2 carry
+///```
+///
+///## Join on storage
+///
 ///```
 /// use caolo_sim::query;
 /// use caolo_sim::init_inmemory_storage;
@@ -117,6 +186,39 @@ macro_rules! query {
 #[macro_export]
 macro_rules! join {
     (
+        [
+            $it0: ident : $row0: ty,
+            $(
+                $its: ident : $rows: ty
+            ),+
+        ]
+    ) => {{
+        #[derive(Clone, Copy)]
+        pub struct Joined<'a> {
+            $it0: &'a $row0,
+            $(
+                $its: &'a $rows
+            ),*
+        }
+
+        join!(@iter $it0, $($its),*)
+            .map(
+                // closure taking id and a nested tuple of pairs
+                |(
+                    id,
+                    join!(@args $it0, $($its),*)
+                 )| {
+                    (id,
+                    Joined {
+                        $it0,
+                        $($its),*
+                    }
+                    )
+                }
+            )
+    }};
+
+    (
         $storage: ident
         $id: ty
         [
@@ -150,6 +252,21 @@ macro_rules! join {
                 }
             )
     }};
+
+    (@iter $it: ident) => {
+        $it
+    };
+
+    (@iter $head: ident,
+            $(
+                $tail: ident
+            ),+
+    ) => {
+        JoinIterator::new(
+            $head,
+            join!(@iter $($tail),*)
+        )
+    };
 
     (@join $storage: ident $id: ty, $row: ty) => {
         // stop the recursion
