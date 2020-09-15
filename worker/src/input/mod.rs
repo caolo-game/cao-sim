e66633d0aacc0f4a7ea676548cc72385367917d6
Daniel Kiss <littlesnorrboy@gmail.com>
2020-09-15 23:08:55 +0200

    Add response sending to command handler

--- worker/src/input/mod.rs
+++ worker/src/input/mod.rs
@@ -1,10 +1,10 @@
 //! Handle inputs received via the message bus
 mod script_update;
 mod structures;
-use cao_messages::{InputMsg, InputPayload};
+use cao_messages::{command::CommandResult, InputMsg, InputPayload};
 use caolo_sim::prelude::*;
 use redis::Commands;
-use slog::{debug, error, Logger};
+use slog::{debug, error, warn, Logger};
 
 pub fn handle_messages(logger: Logger, storage: &mut World, client: &redis::Client) {
     debug!(logger, "handling incoming messages");
@@ -28,35 +28,36 @@ pub fn handle_messages(logger: Logger, storage: &mut World, client: &redis::Clie
     {
         let msg_id = &message.msg_id;
         debug!(logger, "Handling message {}", msg_id);
-        match message.payload {
-            InputPayload::PlaceStructure(cmd) => {
-                structures::place_structure(storage, &cmd)
-                    .map_err(|e| {
-                        error!(logger, "Structure placement {:?} failed {:?}", cmd, e);
-                        // TODO: return error msg
-                    })
-                    .unwrap_or(());
-            }
+        let res = match message.payload {
+            InputPayload::PlaceStructure(cmd) => structures::place_structure(storage, &cmd)
+                .map_err(|e| {
+                    warn!(logger, "Structure placement {:?} failed {:?}", cmd, e);
+                    format!("{}", e)
+                }),
             InputPayload::UpdateScript(update) => {
-                script_update::update_program(logger.clone(), storage, update)
-                    .map_err(|e| {
-                        error!(logger, "Script update failed {:?}", e);
-                        // TODO: return error msg
-                    })
-                    .unwrap_or(());
+                script_update::update_program(logger.clone(), storage, update).map_err(|e| {
+                    warn!(logger, "Script update failed {:?}", e);
+                    format!("{:?}", e)
+                })
             }
             InputPayload::UpdateEntityScript(update) => {
-                script_update::update_entity_script(storage, update)
-                    .map_err(|e| {
-                        error!(logger, "Entity script update failed {:?}", e);
-                        // TODO: return error msg
-                    })
-                    .unwrap_or(());
+                script_update::update_entity_script(storage, update).map_err(|e| {
+                    warn!(logger, "Entity script update failed {:?}", e);
+                    format!("{:?}", e)
+                })
             }
-        }
+        };
+
+        let res = match res {
+            Ok(_) => CommandResult::Ok,
+            Err(err) => CommandResult::Error(err),
+        };
+
+        let payload = rmp_serde::to_vec(&res).expect("Failed to serialize command result");
+
+        let _: () = connection
+            .set_ex(format!("{}", msg_id), payload, 30)
+            .expect("Failed to send command result");
     }
     debug!(logger, "handling incoming messages done");
 }
-
-#[derive(Debug, Clone, Copy)]
-enum UuidDeserializeError {}
