56bb50ea267253ccfefb64d211ba5740cea328e7
Daniel Kiss <littlesnorrboy@gmail.com>
2020-10-02 14:10:14 +0200

    Migrate messaging to cap'n proto

--- worker/src/input/mod.rs
+++ worker/src/input/mod.rs
@@ -1,10 +1,89 @@
 //! Handle inputs received via the message bus
 mod script_update;
 mod structures;
-use cao_messages::{command::CommandResult, InputMsg, InputPayload};
+use anyhow::Context;
+use cao_messages::command_capnp::command::input_message::{self, Which as InputPayload};
+use cao_messages::command_capnp::command_result;
 use caolo_sim::prelude::*;
+use capnp::message::{ReaderOptions, TypedReader};
+use capnp::serialize::try_read_message;
 use redis::Commands;
-use slog::{debug, error, warn, Logger};
+use slog::{debug, error, o, warn, Logger};
+
+type InputMsg = TypedReader<capnp::serialize::OwnedSegments, input_message::Owned>;
+
+fn parse_uuid(id: &cao_messages::command_capnp::uuid::Reader) -> anyhow::Result<uuid::Uuid> {
+    let id = id.get_data().with_context(|| "Failed to get msg id data")?;
+    uuid::Uuid::from_slice(id).with_context(|| "Failed to parse uuid")
+}
+
+/// Write the response and return the msg id
+fn handle_single_message(
+    logger: &Logger,
+    message: InputMsg,
+    storage: &mut World,
+    response: &mut Vec<u8>,
+) -> anyhow::Result<uuid::Uuid> {
+    let message = message.get().with_context(|| "Failed to get typed msg")?;
+    let msg_id = parse_uuid(
+        &message
+            .reborrow()
+            .get_message_id()
+            .with_context(|| "Failed to get msg id")?,
+    )?;
+    let logger = logger.new(o!("msg_id" => format!("{}",msg_id)));
+    debug!(logger, "Handling message");
+    let res = match message
+        .which()
+        .with_context(|| format!("Failed to get msg body of message {}", msg_id))?
+    {
+        InputPayload::PlaceStructure(cmd) => {
+            let cmd = cmd.with_context(|| "Failed to get PlaceStructure message")?;
+            structures::place_structure(logger.clone(), storage, &cmd).map_err(|e| {
+                warn!(logger, "Structure placement failed {:?}", e);
+                format!("{}", e)
+            })
+        }
+        InputPayload::UpdateScript(update) => {
+            let update = update.with_context(|| "Failed to get UpdateScript message")?;
+            script_update::update_program(logger.clone(), storage, &update).map_err(|e| {
+                warn!(logger, "Script update failed {:?}", e);
+                format!("{:?}", e)
+            })
+        }
+        InputPayload::UpdateEntityScript(update) => {
+            let update = update.with_context(|| "Failed to get UpdateEntityScript message")?;
+            script_update::update_entity_script(storage, &update).map_err(|e| {
+                warn!(logger, "Entity script update failed {:?}", e);
+                format!("{:?}", e)
+            })
+        }
+        InputPayload::SetDefaultScript(update) => {
+            let update = update.with_context(|| "Failed to get SetDefaultScript message")?;
+            script_update::set_default_script(storage, &update).map_err(|e| {
+                warn!(logger, "Setting dewfault script failed {:?}", e);
+                format!("{:?}", e)
+            })
+        }
+    };
+
+    let mut msg = capnp::message::Builder::new_default();
+    let mut root = msg.init_root::<command_result::Builder>();
+
+    match res {
+        Ok(_) => {
+            root.set_ok(());
+        }
+        Err(err) => {
+            let mut msg = root.init_error(err.bytes().len() as u32);
+            msg.push_str(err.as_str());
+        }
+    };
+
+    response.clear();
+    capnp::serialize::write_message(response, &msg)?;
+    Ok(msg_id)
+}
 
 pub fn handle_messages(
     logger: Logger,
@@ -12,6 +91,8 @@ pub fn handle_messages(
     connection: &mut redis::Connection,
 ) -> anyhow::Result<()> {
     debug!(logger, "handling incoming messages");
+
+    let mut response = Vec::with_capacity(1_000_000);
 
     // log errors, but otherwise ignore them, so the loop may continue, retrying later
     while let Ok(Some(message)) = connection
@@ -21,52 +102,31 @@ pub fn handle_messages(
         })
         .map::<Option<InputMsg>, _>(|message| {
             message.and_then(|message| {
-                rmp_serde::from_read_ref(message.as_slice())
-                    .map_err(|e| {
-                        error!(logger, "Failed to deserialize message {:?}", e);
-                    })
-                    .ok()
+                try_read_message(
+                    message.as_slice(),
+                    ReaderOptions {
+                        traversal_limit_in_words: 512,
+                        nesting_limit: 64,
+                    },
+                )
+                .map_err(|err| {
+                    error!(logger, "Failed to parse capnp message {:?}", err);
+                })
+                .ok()?
+                .map(|x| x.into_typed())
             })
         })
     {
-        let msg_id = &message.msg_id;
-        debug!(logger, "Handling message {}", msg_id);
-        let res = match message.payload {
-            InputPayload::PlaceStructure(cmd) => structures::place_structure(storage, &cmd)
-                .map_err(|e| {
-                    warn!(logger, "Structure placement {:?} failed {:?}", cmd, e);
-                    format!("{}", e)
-                }),
-            InputPayload::UpdateScript(update) => {
-                script_update::update_program(logger.clone(), storage, update).map_err(|e| {
-                    warn!(logger, "Script update failed {:?}", e);
-                    format!("{:?}", e)
-                })
+        match handle_single_message(&logger, message, storage, &mut response) {
+            Ok(msg_id) => {
+                let _: () = connection
+                    .set_ex(format!("{}", msg_id), response.as_slice(), 30)
+                    .expect("Failed to send command result");
             }
-            InputPayload::UpdateEntityScript(update) => {
-                script_update::update_entity_script(storage, update).map_err(|e| {
-                    warn!(logger, "Entity script update failed {:?}", e);
-                    format!("{:?}", e)
-                })
+            Err(err) => {
+                error!(logger, "Message handling failed, {:?}", err);
             }
-            InputPayload::SetDefaultScript(update) => {
-                script_update::set_default_script(storage, update).map_err(|e| {
-                    warn!(logger, "Setting dewfault script failed {:?}", e);
-                    format!("{:?}", e)
-                })
-            }
-        };
-
-        let res = match res {
-            Ok(_) => CommandResult::Ok,
-            Err(err) => CommandResult::Error(err),
-        };
-
-        let payload = rmp_serde::to_vec(&res).expect("Failed to serialize command result");
-
-        let _: () = connection
-            .set_ex(format!("{}", msg_id), payload, 30)
-            .expect("Failed to send command result");
+        }
     }
     debug!(logger, "handling incoming messages done");
     Ok(())
--- worker/src/input/structures.rs
+++ worker/src/input/structures.rs
@@ -1,8 +1,8 @@
-use cao_messages::command::PlaceStructureCommand;
-use cao_messages::StructureType;
+use cao_messages::command_capnp::{place_structure_command, StructureType};
 use caolo_sim::prelude::*;
 use caolo_sim::tables::JoinIterator;
 use caolo_sim::{join, query};
+use slog::{error, Logger};
 use thiserror::Error;
 use uuid::Uuid;
 
@@ -16,20 +16,40 @@ pub enum PlaceStructureError {
 
     #[error("position {0:?} is taken!")]
     TakenPosition(WorldPosition),
+
+    #[error("Failed to parse Cap'n Proto message {0:?}")]
+    CapnError(capnp::Error),
+
+    #[error("Failed to parse owner id")]
+    OwnerIdError,
 }
 
 pub fn place_structure(
+    logger: Logger,
     storage: &mut World,
-    PlaceStructureCommand {
-        owner,
-        position,
-        ty,
-    }: &PlaceStructureCommand,
+    command: &place_structure_command::Reader,
 ) -> Result<(), PlaceStructureError> {
     let entity_id = storage.insert_entity();
 
-    let pos = Axial::new(position.room_pos.q, position.room_pos.r);
-    let room = Axial::new(position.room.q, position.room.r);
+    let position = command
+        .reborrow()
+        .get_position()
+        .map_err(PlaceStructureError::CapnError)?;
+
+    let pos = position
+        .get_room_pos()
+        .map_err(PlaceStructureError::CapnError)?;
+    let q = pos.get_q();
+    let r = pos.get_r();
+
+    let pos = Axial::new(q, r);
+
+    let room = position
+        .get_room_pos()
+        .map_err(PlaceStructureError::CapnError)?;
+    let q = room.get_q();
+    let r = room.get_r();
+    let room = Axial::new(q, r);
 
     let position = WorldPosition { room, pos };
 
@@ -52,23 +72,37 @@ pub fn place_structure(
         return Err(PlaceStructureError::TakenPosition(position));
     }
 
+    let ty = command.reborrow().get_ty().map_err(|err| {
+        PlaceStructureError::CapnError(capnp::Error::failed(format!(
+            "failed to get StructureType {:?}",
+            err
+        )))
+    })?;
+
+    let owner = command
+        .reborrow()
+        .get_owner()
+        .map_err(PlaceStructureError::CapnError)?
+        .get_data()
+        .map_err(PlaceStructureError::CapnError)?;
+    let owner = uuid::Uuid::from_slice(owner).map_err(|err| {
+        error!(logger, "Failed to parse owner id {:?}", err);
+        PlaceStructureError::OwnerIdError
+    })?;
     match ty {
         StructureType::Spawn => {
             // a player may only have 1 spawn atm
             let has_spawn = join!(
                 storage
                 EntityId
-                [
-                    spawn: SpawnComponent,
-                    owner: OwnedEntity
-                ]
+                [ spawn: SpawnComponent, owner: OwnedEntity ]
             )
-            .find(|(_, (_, OwnedEntity { ref owner_id }))| &owner_id.0 == owner)
+            .find(|(_, (_, OwnedEntity { ref owner_id }))| &owner_id.0 == &owner)
             .map(|(id, _)| id);
 
             if let Some(spawn_id) = has_spawn {
                 return Err(PlaceStructureError::UserHasSpawn {
-                    user_id: *owner,
+                    user_id: owner,
                     spawn_id,
                 });
             }
@@ -84,7 +118,7 @@ pub fn place_structure(
         }
     }
 
-    let owner_id = UserId(*owner);
+    let owner_id = UserId(owner);
     query!(
         mutate
         storage
