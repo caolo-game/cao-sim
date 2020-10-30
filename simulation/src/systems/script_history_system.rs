f30781266ae32e6908aceaa68a7c02d4df820083
Daniel Kiss <littlesnorrboy@gmail.com>
2020-10-30 17:14:50 +0100

    Clear history entries

--- simulation/src/systems/script_history_system.rs
+++ simulation/src/systems/script_history_system.rs
@@ -14,6 +14,7 @@ pub fn update((mut history_intents, mut history_table): Mut, _: Const) {
     profile!("ScriptHistorySystem update");
 
     let Intents(intents) = mem::take(&mut *history_intents);
+    history_table.clear();
     for intent in intents {
         history_table.insert_or_update(intent.entity_id, ScriptHistory(intent.payload));
     }
