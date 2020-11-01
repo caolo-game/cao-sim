7bf56a77a6819c0312004fe86be81ed13ddeb0e1
Daniel Kiss <littlesnorrboy@gmail.com>
2020-11-01 18:49:28 +0100

    Init

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,10 +1,8 @@
 mod config;
 mod init;
 mod input;
-mod output;
 
 use anyhow::Context;
-use cao_messages::world_capnp::world_state;
 use caolo_sim::{executor::mp_executor, executor::Executor, prelude::*};
 use mp_executor::MpExecutor;
 use slog::{debug, error, info, o, trace, warn, Drain, Logger};
@@ -13,7 +11,6 @@ use std::{
     env,
     time::{Duration, Instant},
 };
-use thiserror::Error;
 
 #[cfg(feature = "jemallocator")]
 #[global_allocator]
@@ -40,193 +37,73 @@ fn tick(logger: Logger, exc: &mut impl Executor, storage: &mut World) {
         .expect("Failed to forward game state")
 }
 
-fn send_config(
+async fn send_schema<'a>(
     logger: Logger,
-    client: &redis::Client,
-    storage: &World,
-    game_conf: &config::GameConfig,
+    connection: impl sqlx::Executor<'a, Database = sqlx::Postgres>,
 ) -> anyhow::Result<()> {
-    debug!(logger, "Sending config");
-
-    let rooms_props = UnwrapView::<ConfigKey, RoomProperties>::new(storage);
-
-    let conf = serde_json::json!({
-        "roomProperties" : *rooms_props,
-        "gameConfig": game_conf
-    });
-
-    let payload = serde_json::to_vec(&conf)?;
-
-    let mut con = client.get_connection()?;
-    redis::pipe()
-        .cmd("SET")
-        .arg("SIM_CONFIG")
-        .arg(payload)
-        .query(&mut con)
-        .with_context(|| "Failed to send config")?;
-
-    debug!(logger, "Sending config - done");
-
-    Ok(())
-}
-
-fn send_world(
-    logger: Logger,
-    storage: &World,
-    connection: &mut redis::Connection,
-) -> anyhow::Result<()> {
-    debug!(logger, "Sending world state");
-
-    let mut msg = capnp::message::Builder::new_default();
-    let mut root = msg.init_root::<world_state::Builder>();
-
-    output::build_bots(FromWorld::new(storage), &mut root);
-    output::build_resources(FromWorld::new(storage), &mut root);
-    output::build_structures(FromWorld::new(storage), &mut root);
-    output::build_script_history(FromWorld::new(storage), &mut root);
-    // FIXME: causes panic
-    // output::build_logs(FromWorld::new(storage), &mut root);
-
-    let mut payload = Vec::with_capacity(1_000_000);
-    capnp::serialize::write_message(&mut payload, &msg)?;
-
-    debug!(logger, "sending {} bytes", payload.len());
-
-    redis::pipe()
-        .cmd("SET")
-        .arg("WORLD_STATE")
-        .arg(payload)
-        .query(connection)
-        .with_context(|| "Failed to send WORLD_STATE")?;
-
-    debug!(logger, "Sending world state done");
-    Ok(())
-}
-
-#[derive(Debug, Clone, Error)]
-pub enum TerrainSendFail {
-    #[error("RoomProperties were not set")]
-    RoomPropertiesNotSet,
-}
-
-async fn send_terrain(logger: &Logger, storage: &World, client: &PgPool) -> anyhow::Result<()> {
-    use serde_json::json;
-    debug!(logger, "sending terrain ");
-
-    let room_properties = storage
-        .view::<ConfigKey, RoomProperties>()
-        .reborrow()
-        .value
-        .as_ref()
-        .ok_or(TerrainSendFail::RoomPropertiesNotSet)?;
-
-    let room_radius = room_properties.radius;
-
-    let mut tx = client.begin().await?;
-    sqlx::query("DELETE FROM world_map")
-        .execute(&mut tx)
-        .await?;
-
-    for (Room(room), tiles) in output::iter_rooms_terrain(FromWorld::new(storage)) {
-        trace!(
-            logger,
-            "sending room {:?} terrain, len: {}",
-            room,
-            tiles.len()
-        );
-
-        let q = room.q;
-        let r = room.r;
-
-        let payload = json!( {
-            "tiles": tiles,
-            "roomProperties": {
-                "roomRadius": room_radius,
-                "roomId": room,
-            }
-        });
-
-        sqlx::query(
-            r#"
-            INSERT INTO world_map (q, r, payload)
-            VALUES ($1, $2, $3)"#,
-        )
-        .bind(q)
-        .bind(r)
-        .bind(payload)
-        .execute(&mut tx)
-        .await?;
-    }
-    tx.commit().await?;
-
-    debug!(logger, "sending terrain done");
-    Ok(())
-}
-
-fn send_schema(logger: Logger, client: &redis::Client) -> anyhow::Result<()> {
-    use cao_messages::script_capnp::schema;
-    debug!(logger, "Sending schema");
-    let schema = caolo_sim::scripting_api::make_import();
-    let imports = schema.imports();
-
-    let mut msg = capnp::message::Builder::new_default();
-    let mut root = msg.init_root::<schema::Builder>();
-
-    let len = imports.len();
-    let mut cards = root.reborrow().init_cards(len as u32);
-    imports.iter().enumerate().for_each(|(i, import)| {
-        let import = &import.desc;
-        let mut card = cards.reborrow().get(i as u32);
-        card.set_name(import.name);
-        card.set_description(import.description);
-        card.set_ty(
-            serde_json::to_string(&import.ty)
-                .expect("Set card type")
-                .as_str(),
-        );
-        {
-            let len = import.input.len();
-            let mut inputs = card.reborrow().init_input(len as u32);
-            import
-                .input
-                .iter()
-                .enumerate()
-                .for_each(|(i, inp)| inputs.set(i as u32, inp));
-        }
-        {
-            let len = import.output.len();
-            let mut outputs = card.reborrow().init_output(len as u32);
-            import
-                .output
-                .iter()
-                .enumerate()
-                .for_each(|(i, inp)| outputs.set(i as u32, inp));
-        }
-        {
-            let len = import.constants.len();
-            let mut constants = card.reborrow().init_constants(len as u32);
-            import
-                .constants
-                .iter()
-                .enumerate()
-                .for_each(|(i, inp)| constants.set(i as u32, inp));
-        }
-    });
-
-    let mut payload = Vec::with_capacity(1_000_000);
-    capnp::serialize::write_message(&mut payload, &msg)?;
-
-    let mut con = client.get_connection()?;
-
-    redis::pipe()
-        .cmd("SET")
-        .arg("SCHEMA")
-        .arg(payload)
-        .query(&mut con)
-        .with_context(|| "Failed to set SCHEMA")?;
-
-    debug!(logger, "Sending schema done");
-    Ok(())
+    todo!()
+    // use cao_messages::script_capnp::schema;
+    // debug!(logger, "Sending schema");
+    // let schema = caolo_sim::scripting_api::make_import();
+    // let imports = schema.imports();
+    //
+    // let mut msg = capnp::message::Builder::new_default();
+    // let mut root = msg.init_root::<schema::Builder>();
+    //
+    // let len = imports.len();
+    // let mut cards = root.reborrow().init_cards(len as u32);
+    // imports.iter().enumerate().for_each(|(i, import)| {
+    //     let import = &import.desc;
+    //     let mut card = cards.reborrow().get(i as u32);
+    //     card.set_name(import.name);
+    //     card.set_description(import.description);
+    //     card.set_ty(
+    //         serde_json::to_string(&import.ty)
+    //             .expect("Set card type")
+    //             .as_str(),
+    //     );
+    //     {
+    //         let len = import.input.len();
+    //         let mut inputs = card.reborrow().init_input(len as u32);
+    //         import
+    //             .input
+    //             .iter()
+    //             .enumerate()
+    //             .for_each(|(i, inp)| inputs.set(i as u32, inp));
+    //     }
+    //     {
+    //         let len = import.output.len();
+    //         let mut outputs = card.reborrow().init_output(len as u32);
+    //         import
+    //             .output
+    //             .iter()
+    //             .enumerate()
+    //             .for_each(|(i, inp)| outputs.set(i as u32, inp));
+    //     }
+    //     {
+    //         let len = import.constants.len();
+    //         let mut constants = card.reborrow().init_constants(len as u32);
+    //         import
+    //             .constants
+    //             .iter()
+    //             .enumerate()
+    //             .for_each(|(i, inp)| constants.set(i as u32, inp));
+    //     }
+    // });
+    //
+    // let mut payload = Vec::with_capacity(1_000_000);
+    // capnp::serialize::write_message(&mut payload, &msg)?;
+    //
+    // let mut con = client.get_connection()?;
+    //
+    // redis::pipe()
+    //     .cmd("SET")
+    //     .arg("SCHEMA")
+    //     .arg(payload)
+    //     .query(&mut con)
+    //     .with_context(|| "Failed to set SCHEMA")?;
+    //
+    // debug!(logger, "Sending schema done");
 }
 
 fn main() {
@@ -281,6 +158,8 @@ fn main() {
         script_chunk_size,
         tick_freq
     );
+    let database_url = env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "postgres://postgres:admin@localhost:5432/caolo".to_owned());
 
     info!(logger, "Creating cao executor");
     let mut executor = sim_rt
@@ -288,7 +167,7 @@ fn main() {
             &sim_rt,
             logger.clone(),
             mp_executor::ExecutorOptions {
-                redis_url: redis_url.clone(),
+                postgres_url: database_url.clone(),
                 queen_mutex_expiry_ms,
                 script_chunk_size,
                 expected_frequency: chrono::Duration::milliseconds(
@@ -314,50 +193,35 @@ fn main() {
         .block_on(executor.update_role())
         .expect("Update role");
 
-    let redis_client = redis::Client::open(redis_url.as_str()).expect("Redis client");
-    let pg_pool = sim_rt
-        .block_on(PgPool::new(&env::var("DATABASE_URL").unwrap_or_else(
-            |_| "postgres://postgres:admin@localhost:5432/caolo".to_owned(),
-        )))
-        .expect("Connect to PG");
-
     if executor.is_queen() {
         init::init_storage(logger.clone(), &mut storage, &game_conf);
-        send_config(
-            logger.clone(),
-            &redis_client,
-            &*storage.as_ref(),
-            &game_conf,
-        )
-        .expect("Send config");
 
         sim_rt
-            .block_on(send_terrain(&logger, &*storage.as_ref(), &pg_pool))
-            .expect("Send terrain");
+            .block_on(async move {
+                let pg_conn = sqlx::postgres::PgPoolOptions::new()
+                    .connect(database_url.as_str())
+                    .await
+                    .expect("Connect to PG");
 
-        send_schema(logger.clone(), &redis_client).expect("Send schema");
+                send_schema(logger.clone(), &mut pg_conn.acquire().await?).await
+            })
+            .expect("Send schema");
     }
 
     sentry::capture_message(
-        "Caolo Worker initialization complete! Starting the game loop",
+        format!(
+            "Caolo Worker {} initialization complete! Starting the game loop",
+            executor.tag
+        )
+        .as_str(),
         sentry::Level::Info,
     );
-    let mut redis_connection = redis_client
-        .get_connection()
-        .expect("Get redis connection failed");
 
     loop {
         let start = Instant::now();
 
         tick(logger.clone(), &mut executor, &mut storage);
 
-        if executor.is_queen() {
-            send_world(logger.clone(), &storage, &mut redis_connection)
-                .map_err(|err| {
-                    error!(logger, "Failed to send world {:?}", err);
-                })
-                .unwrap_or(());
-        }
         let mut sleep_duration = tick_freq
             .checked_sub(Instant::now() - start)
             .unwrap_or_else(|| Duration::from_millis(0));
